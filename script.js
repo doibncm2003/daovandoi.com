8ab163d2c909ab82d707c7dfb723dc4331569546
doibncm2003 <152420161+doibncm2003@users.noreply.github.com>
2024-02-02 19:09:34 +0700

    8

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const menuToggle = document.querySelector('.menu-toggle');
+//     const nav = document.querySelector('nav');
+
+//     menuToggle.addEventListener('click', function () {
+//         nav.classList.toggle('active');
+//     });
+// });
+
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.menu-toggle');
     const nav = document.querySelector('nav');
 
     menuToggle.addEventListener('click', function () {
         nav.classList.toggle('active');
+        menuToggle.classList.toggle('open');
     });
-});
+})
