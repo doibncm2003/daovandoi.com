9746f5cad3b71a3a4902fd525dce24ead3b5c4bd
doibncm2003 <152420161+doibncm2003@users.noreply.github.com>
2024-02-22 23:25:46 +0700

    66

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ document.addEventListener('DOMContentLoaded', function () {
         menuToggle.classList.toggle('open');
     });
 });
+/////
+document.addEventListener('DOMContentLoaded', function() {
+    const raindropsContainer = document.querySelector('.raindrops-container');
+
+    for (let i = 5; i < 20; i++) { // Tăng số lượng giọt mưa
+        createRaindrop();
+    }
+
+    function createRaindrop() {
+        const raindrop = document.createElement('div');
+        raindrop.classList.add('raindrop');
+        raindrop.style.left = Math.random() * 100 + 'vw';
+        raindrop.style.animationDuration = (Math.random() * 2 + 2) + 's';
+        raindropsContainer.appendChild(raindrop);
+    }
+});
